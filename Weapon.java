a17c4d4cab31d4176571ef99dcc0b03153f293e4
BidoofOverlord119 <79423097+BidoofOverlord119@users.noreply.github.com>
2023-04-12 11:14:20 -0400

    Create Weapon.java

---
+++ Weapon.java
@@ -0,0 +1,65 @@
+import javax.print.DocFlavor.STRING;
+
+public class Weapon 
+{
+   //using damageFormatException;
+   private String weaponClass;
+   private String weaponName;
+   private double damagePerhit;
+
+   public Weapon()
+   {
+   }
+   public Weapon( String wClass, String wName, Double damage ) throws damageFormatException
+   {
+      weaponClass = wClass;
+      weaponName = wName;
+      damagePerhit = damage;
+   } 
+   
+   
+   public void setweaponClass( String wName )
+   {
+      weaponClass = wName;
+   } 
+   
+   
+   public String getweaponClass()
+   {
+      return weaponClass;
+   } 
+   
+   
+   public void setweaponName( String wName )
+   {
+      weaponName = wName;
+   } 
+   
+   public String getweaponName()
+   {
+      return weaponName;
+   }
+
+   public void setdamagePerhit( String damage ) throws damageFormatException
+   {   
+      
+      damagePerhit = damage;
+      
+   } 
+
+   
+
+   public String getdamagePerhit()
+   {
+      return damagePerhit;
+   }
+
+   public String toString()
+   {
+      return String.format( "%s %s\nstats:\n\tdamage: %d", 
+      getweaponName(), getweaponClass(), getdamagePerhit() );
+   }
+
+} // end abstract class Weapon
+
+
