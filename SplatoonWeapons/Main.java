62a4c48eadaa5829da865efdd5234c57e87b06ff
Aluminite <10791181+Aluminite@users.noreply.github.com>
2023-04-20 21:23:53 -0400

    New main method that tests all the weapons with user parameters

--- SplatoonWeapons/Main.java
+++ SplatoonWeapons/Main.java
@@ -1,4 +1,21 @@
 package SplatoonWeapons;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("What is the distance of the target? ");
+        double targetDistance = input.nextDouble();
+        System.out.print("What is the X offset of the target (0 for none)? ");
+        double targetXOffset = input.nextDouble();
+        System.out.print("How many frames should the weapon fire for (60 FPS)? ");
+        int time = input.nextInt();
+        for (Weapon w: Weapons.weapons) {
+            System.out.println(w);
+            System.out.printf("Weapon dealt %.1f damage over %d frames (%.2f seconds) to a target %.1f units away, " +
+                    "with X offset %.2f%n", w.calculateDamageOverTime(targetDistance, targetXOffset, time) / 10.0,
+                    time, time / 60.0, targetDistance, targetXOffset);
+        }
+    }
 }
