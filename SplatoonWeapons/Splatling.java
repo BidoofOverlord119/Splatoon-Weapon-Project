c99aa2508ca74ddf0016240930b72285ff4cda60
BidoofOverlord119 <79423097+BidoofOverlord119@users.noreply.github.com>
2023-04-21 13:20:48 -0400

    Update Splatling.java
    Deleted calc DOT because in theory there's no need to change it if we're going based on time as opposed to charge,
    It may get put back if we unextend  splatling from shooter, but for now its unneeded.

--- SplatoonWeapons/Splatling.java
+++ SplatoonWeapons/Splatling.java
@@ -13,21 +13,18 @@ public class Splatling extends Shooter {
 
     public Splatling(String weaponName, int baseDamage, int shotInterval, int[] falloffStats, double[] shotDeviationStats, double[] velocityStats, int chargeRing1, int chargeRing2) {
         super(weaponName,baseDamage,shotInterval,falloffStats,shotDeviationStats,velocityStats);
+        if (!(chargeRing1 >= 0)) {
+            throw new IllegalArgumentException("Charge Time for Ring 1 must be at least 0");
+        }
+        if (!(chargeRing2 >= 0)) {
+            throw new IllegalArgumentException("Charge Time for Ring 2 must be at least 0");
+        }
+        if (!(chargeRing2 > chargeRing1)) {
+            throw new IllegalArgumentException("Charge Time for Ring 2 must be greater than that of the Charge Time for Ring 1");
+        }
         this.chargeRing1 = chargeRing1;
         this.chargeRing2 = chargeRing2;
     }
 
-    @Override
-    public int calculateDamageOverTime(double targetDistance, double targetXOffset, int time) {
-        if (time == 0) return 0;
-        int damageDealt = 0;
-        int numShots = ((time - 1) / getShotInterval()) + 1; // always shoots on the first frame
-        for (int i = 0; i < numShots; i++) {
-            int currentDamage = calculateHit(targetDistance, targetXOffset, i);
-            if (currentDamage != -1) {
-                damageDealt += currentDamage;
-            }
-        }
-        return damageDealt;
-    }
+    
 }
