0a108bda326e3424b620ec0542e2a104dcf8f32d
Aluminite <10791181+Aluminite@users.noreply.github.com>
2023-04-26 13:31:24 -0400

    Functional Blaster

--- SplatoonWeapons/Blaster.java
+++ SplatoonWeapons/Blaster.java
@@ -1,106 +1,88 @@
 package SplatoonWeapons;
-import java.util.Dictionary;
-import java.util.Hashtable;
 
 public class Blaster implements Weapon {
-    /* 
-     * The name given to the weapon.
-    */
     private final String weaponName;
-
-    /* 
-     * The damage dealt by the projectile on a direct impact.
-    */
     private final int baseDamage;
-    
-    /* 
-     * The distance the projectile travels.
-    */
     private final double mainRange;
-
-    /* 
-     * Damage Dealt in the close radius of the blast caused at the end of the projectile's lifespan
-    */
-    private final int blastDamageNear;
-
-    /* 
-     * Damage Dealt in the far radius of the blast caused at the end of the projectile's lifespan
-    */
-    private final int blastDamageFar;
-
-    /* 
-     * The near radius of the blast caused at the end of the projectile's lifespan
-    */
-    private final double blastRangeNear;
-
-    /* 
-     * The far radius of the blast caused at the end of the projectile's lifespan
-    */
-    private final double blastRangeFar;
-
-    
-    
-    /* 
-     * Fire Rate variable
-    */
     private final int shotInterval;
+    private final int firstShotDelay;
+    private final int blastDamageNear;
+    private final int blastDamageFar;
+    private final double blastNearRadius;
+    private final double blastFarRadius;
 
-    public Blaster(String weaponName, int baseDamage, int blastDamageNear, int blastDamageFar,
-                   double mainRange, double blastRangeNear, double blastRangeFar, int shotInterval) {
+    /**
+     * Constructor for the Blaster object.
+     *
+     * @param weaponName      Friendly name of the weapon.
+     * @param baseDamage      Base damage of the weapon, from ValueMax.
+     * @param mainRange       Range of the weapon. Calculated based on the velocity stats.
+     *                        (SpawnSpeed * GoStraightToBrakeStateFrame) + (2 * GoStraightStateEndMaxSpeed)
+     * @param shotInterval    Time (in frames) between shots, from RepeatFrame.
+     * @param firstShotDelay  Time (in frames) before the first shot fires. From PreDelayFrame_HumanShot.
+     * @param blastDamageNear Maximum blast damage when blast is close, from DistanceDamage[0].Damage.
+     * @param blastDamageFar  Minimum blast damage when blast is far, from DistanceDamage[1].Damage.
+     * @param blastNearRadius Radius in which the maximum blast damage will occur, from DistanceDamage[0].Distance.
+     * @param blastFarRadius  Radius in which any blast damage will occur, from DistanceDamage[1].Distance.
+     */
+    public Blaster(String weaponName, int baseDamage, double mainRange, int shotInterval, int firstShotDelay,
+                   int blastDamageNear, int blastDamageFar, double blastNearRadius, double blastFarRadius) {
         this.weaponName = weaponName;
         this.baseDamage = baseDamage;
         this.mainRange = mainRange;
         this.shotInterval = shotInterval;
+        this.firstShotDelay = firstShotDelay;
         this.blastDamageNear = blastDamageNear;
         this.blastDamageFar = blastDamageFar;
-        this.blastRangeNear = blastRangeNear;
-        this.blastRangeFar = blastRangeFar;
+        this.blastNearRadius = blastNearRadius;
+        this.blastFarRadius = blastFarRadius;
     }
 
-    public int calculateDamage(double targetDistance,double targetXOffset) { // chargeState should be 0 to 1 and represents charge percentage
-        if (!(targetDistance >= 0)) {
-            throw new IllegalArgumentException("Target distance must be at least 0");
-        }
-        if (targetDistance<=mainRange) return baseDamage;
-        double targetYOffset = targetDistance - mainRange;
-        if (targetYOffset<=blastRangeNear||targetXOffset<=blastRangeNear){
+    public int calculateSplashDamage(double distanceToTarget) {
+        if (distanceToTarget <= blastNearRadius) {
             return blastDamageNear;
+        } else if (distanceToTarget >= blastFarRadius) {
+            return 0;
+        } else {
+            // damage scales linearly from blastDamageNear to blastDamageFar
+            int damageDifference = blastDamageNear - blastDamageFar;
+            double rangeDifference = blastFarRadius - blastNearRadius;
+            return (int) ((damageDifference * ((distanceToTarget - blastNearRadius) / rangeDifference)) + blastDamageFar);
         }
-        else if (targetYOffset<=blastRangeFar||targetXOffset<=blastRangeFar) {
-            return blastDamageFar;
-        }
-        return 0;
-            
     }
 
-    /* public int calculateHit(double targetDistance, double targetXOffset, double chargeState) {
+    public int calculateHit(double targetDistance, double targetXOffset) {
         if (!(targetDistance >= 0)) {
             throw new IllegalArgumentException("Target distance must be at least 0");
         }
 
         double targetSize = 0.7;
-        double range = mainRange;
-        if (range < targetDistance) {
-            return -1;
-        }
-        // chargers don't have shot deviation, so if the x offset is more than half the target's size
-        // in either direction it will miss
-        if (((targetSize / 2.0) - Math.abs(targetXOffset)) < 0) {
-            return -1;
+        // blasters don't have shot deviation, so if the x offset is more than half the target's size
+        // in either direction it will cause an indirect hit or a miss
+        if (mainRange < targetDistance || (((targetSize / 2.0) - Math.abs(targetXOffset)) < 0)) {
+            // shot and target form a right triangle
+            double distanceToTarget = Math.sqrt(Math.pow(mainRange - targetDistance, 2) + Math.pow(targetXOffset, 2));
+            return calculateSplashDamage(distanceToTarget);
         }
 
-        return calculateDamage(chargeState);
-    } */
+        return getBaseDamage();
+    }
 
-
-
+    /**
+     * Calculates damage dealt by the weapon to a target over a certain amount of time.
+     *
+     * @param targetDistance Distance from the player to the target, in distance units.
+     * @param targetXOffset  Left-right offset of the target, in distance units.
+     * @param time           How long to shoot for, in frames.
+     * @return Total damage dealt.
+     */
     @Override
     public int calculateDamageOverTime(double targetDistance, double targetXOffset, int time) {
         if (time == 0) return 0;
         int damageDealt = 0;
-        int numShots = ((time - 1) / shotInterval) + 1; // always shoots on the first frame
-        for (int i = 0; i<numShots;i++) {
-            damageDealt+= calculateDamage(targetDistance, targetXOffset); // change i to Calculate hit when implemented. 
+        int numShots = ((time - firstShotDelay - 1) / shotInterval) + 1; //same as shooter but with delay for first shot
+        for (int i = 0; i < numShots; i++) {
+            damageDealt += calculateHit(targetDistance, targetXOffset);
         }
         return damageDealt;
     }
@@ -111,19 +93,16 @@ public class Blaster implements Weapon {
                 getWeaponName(), getBaseDamage() / 10.0, getBaseFireRate());
     }
 
-    //@Override
+    @Override
     public String getWeaponName() {
         return weaponName;
     }
 
-    //@Override
+    @Override
     public int getBaseDamage() {
         return baseDamage;
     }
 
-    public double getmainRange() {
-        return mainRange;
-    }
     @Override
     public double getBaseFireRate() {
         return 60 / (double) shotInterval;
--- SplatoonWeapons/Weapons.java
+++ SplatoonWeapons/Weapons.java
@@ -2,65 +2,70 @@ package SplatoonWeapons;
 
 public class Weapons {
     public static final Shooter splattershot = new Shooter(
-        "Splattershot", 360, 6,
-        8, 40, 180,
-        0.01, 0.25, 0.01, 6.0,
-        2.2, 4, 1.4495
+            "Splattershot", 360, 6,
+            8, 40, 180,
+            0.01, 0.25, 0.01, 6.0,
+            2.2, 4, 1.4495
     );
 
     public static final Shooter jetSquelcher = new Shooter(
-        "Jet Squelcher", 320, 8,
-        9, 25, 160,
-        0.02, 0.25, 0.015, 2.5,
-        3.36, 5, 2.232
+            "Jet Squelcher", 320, 8,
+            9, 25, 160,
+            0.02, 0.25, 0.015, 2.5,
+            3.36, 5, 2.232
     );
 
     public static final Shooter aerospray = new Shooter(
-        "Aerospray MG", 240, 4,
-        8, 24, 120,
-        0.06, 0.5, 0.03, 13,
-        2.2, 3, 1.8945
+            "Aerospray MG", 240, 4,
+            8, 24, 120,
+            0.06, 0.5, 0.03, 13,
+            2.2, 3, 1.8945
     );
 
     public static final Charger splatCharger = new Charger(
-        "Splat Charger", 1600, 400, 800,
-        24.037, 9.033, 24.037,
-        60
+            "Splat Charger", 1600, 400, 800,
+            24.037, 9.033, 24.037,
+            60
     );
 
     public static final Charger eliter4k = new Charger(
-        "E-Liter 4K", 1800, 400, 800,
-        29.05, 9.045, 29.05,
-        92
+            "E-Liter 4K", 1800, 400, 800,
+            29.05, 9.045, 29.05,
+            92
     );
 
     public static final Charger bamboozler = new Charger(
-        "Bamboozler 14 Mk I", 850, 850, 300,
-        19.564, 19.564, 19.564,
-        20
+            "Bamboozler 14 Mk I", 850, 850, 300,
+            19.564, 19.564, 19.564,
+            20
     );
     public static final Splatling heavySplatling = new Splatling(
-        "Heavy Splatling", 300, 4,
-        11, 19, 150,
-        0.01, 0.33, 0.02, 
-        3.3, 1.05, 8, 
-        1.5105, 50, 75,72,2,300
-        );
+            "Heavy Splatling", 300, 4,
+            11, 19, 150,
+            0.01, 0.33, 0.02,
+            3.3, 1.05, 8,
+            1.5105, 50, 75, 72, 2, 300
+    );
 
-        public static final Splatling hydraSplatling = new Splatling(
-        "Hydra Splatling", 320, 4,
-        11, 19, 160,
-        0.01, 0.30, 0.02, 
-        3, 1.05, 8, 
-        1.94, 120, 150,120,4,400);
+    public static final Splatling hydraSplatling = new Splatling(
+            "Hydra Splatling", 320, 4,
+            11, 19, 160,
+            0.01, 0.30, 0.02,
+            3, 1.05, 8,
+            1.94, 120, 150, 120, 4, 400);
 
-        public static final Splatling miniSplatling = new Splatling(
-        "Mini Splatling", 320, 4,
-        11, 19, 160,
-        0.01, 0.30, 0.02, 
-        4, 1.05, 8, 
-        1.105, 20, 30,36,2,320);
+    public static final Splatling miniSplatling = new Splatling(
+            "Mini Splatling", 320, 4,
+            11, 19, 160,
+            0.01, 0.30, 0.02,
+            4, 1.05, 8,
+            1.105, 20, 30, 36, 2, 320);
+
+    public static final Blaster blaster = new Blaster(
+            "Blaster", 1250, (0.945 * 9) + (0.9131 * 2), 50, 10,
+            700, 500, 0.94, 3.3
+    );
 
     public static final Weapon[] weapons = new Weapon[]{splattershot, jetSquelcher, aerospray, splatCharger,
-    eliter4k, bamboozler,heavySplatling,hydraSplatling};
+            eliter4k, bamboozler, heavySplatling, hydraSplatling, blaster};
 }
